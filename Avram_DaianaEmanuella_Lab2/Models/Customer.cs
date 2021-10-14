6ba92658b3fe08272feafa9b0196194872d26f5c
Daiana Avram <daiana.avram@devnest.ro>
2021-10-14 19:54:14 +0300

    Add project files.

---
+++ Avram_DaianaEmanuella_Lab2/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Avram_DaianaEmanuella_Lab2.Models
+{
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CustomerID { get; set; }
+        public String Name { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+        
+        public ICollection<Order> Orders { get; set; }
+    }
+}
